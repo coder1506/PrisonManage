21beab18a6572b91fec217ddb2d4908874846bdb
coder1506 <duc5061@gmail.com>
2024-06-29 14:37:39 +0700

    init

---
+++ src/components/data.js
@@ -0,0 +1,23 @@
+// dữ liệu fix cứng dạng json
+const prisons = [
+    {
+      id: 1,
+      code: 1,
+      name: "Phòng giam A",
+      quantity: 10,
+    },
+    {
+      id: 2,
+      code: 2,
+      name: "Phòng giam B",
+      quantity: 20,
+    },
+    {
+      id: 3,
+      code: 3,
+      name: "Phòng giam C",
+      quantity: 30,
+    },
+  ];
+  
+  export default prisons;
